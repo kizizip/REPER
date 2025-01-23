c2608eec82629045f12389512404572916f3664e
DESKTOP-4QCTMKG\SSAFY <ajh8844@naver.com>
2025-01-23 15:24:15 +0900

    [feat] front - 프로젝트생성

---
+++ reper_front/app/src/main/java/com/ssafy/reper/data/local/SharedPreferencesUtil.kt
@@ -0,0 +1,65 @@
+//package com.ssafy.smartstore_jetpack.data.local
+//
+//import android.content.Context
+//import android.content.SharedPreferences
+//import android.util.Log
+//import com.google.gson.Gson
+//import com.ssafy.smartstore_jetpack.data.model.dto.UserTodayeat
+//
+//private const val TAG = "SharedPreferencesUtil_싸피"
+//class SharedPreferencesUtil (context: Context) {
+//    val SHARED_PREFERENCES_NAME = "smartstore_preference"
+//    val COOKIES_KEY_NAME = "cookies"
+//
+//    var preferences: SharedPreferences =
+//        context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+//
+//    //사용자 정보 저장
+//    fun addUser(user: UserTodayeat){
+//        val editor = preferences.edit()
+//        editor.putString("id", user.id)
+//        editor.putString("name", user.name)
+//        editor.putString("address", user.address)
+//        editor.putString("phone", user.phone)
+//
+//        Log.d(TAG, "addUser: ${user.address}, ${user.phone}")
+//        editor.apply()
+//    }
+//
+//    fun getUser(): UserTodayeat {
+//        val id = preferences.getString("id", "")
+//        val name = preferences.getString("name", "")
+//        val pass = preferences.getString("pass", "")
+//        val phone = preferences.getString("phone", "")
+//        val address = preferences.getString("address", "")
+//
+//        if (id != ""){
+//            return UserTodayeat(id!! ,name!!, pass!!, phone!!, address!!)
+//        }else{
+//            return UserTodayeat("","","","","")
+//        }
+//    }
+//
+//    fun deleteUser(){
+//        //preference 지우기
+//        val editor = preferences.edit()
+//        editor.clear()
+//        editor.apply()
+//    }
+//
+//    fun addUserCookie(cookies: HashSet<String>) {
+//        val editor = preferences.edit()
+//        editor.putStringSet(COOKIES_KEY_NAME, cookies)
+//        editor.apply()
+//    }
+//
+//    fun getUserCookie(): MutableSet<String>? {
+//        return preferences.getStringSet(COOKIES_KEY_NAME, HashSet())
+//    }
+//
+//    fun deleteUserCookie() {
+//        preferences.edit().remove(COOKIES_KEY_NAME).apply()
+//    }
+//
+//
+//}
