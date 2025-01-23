1136976a5d4ee8e910b2520ace40ebb4f13b208f
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-01-23 15:49:13 +0900

    [style] front - ui폴더 생성

--- reper_front/app/src/main/java/com/ssafy/reper/ui/MainActivity.kt
+++ reper_front/app/src/main/java/com/ssafy/reper/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ssafy.reper
+package com.ssafy.reper.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ssafy.reper.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
