11cdac5b7dbf1b803d18bc27d5467bccba939611
이서현 <pado9589@gmail.com>
2025-01-24 16:08:33 +0900

    [feat] back - Swagger UI 타이틀 및 설명 파일 (적용 안 됨, 수정 필요)

---
+++ reper/src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Reper API")
+                .description("Reper 서비스의 REST Api입니다.")
+                .version("1.0.0");
+    }
+}
