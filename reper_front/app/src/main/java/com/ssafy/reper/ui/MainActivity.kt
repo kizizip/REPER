907f10c48ce4dc8d8dd5c4d95a40de24a94d64d3
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-01-25 23:13:50 +0900

    [feat] front - 화면이동을 위한 MainActivity 버튼 생성

--- reper_front/app/src/main/java/com/ssafy/reper/ui/MainActivity.kt
+++ reper_front/app/src/main/java/com/ssafy/reper/ui/MainActivity.kt
@@ -1,23 +1,84 @@
 package com.ssafy.reper.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ssafy.reper.R
+import com.ssafy.reper.databinding.ActivityMainBinding
+import com.ssafy.reper.ui.boss.BossFragment
+import com.ssafy.reper.ui.boss.NoticeManageFragment
+import com.ssafy.reper.ui.boss.RecipeManageFragment
+import com.ssafy.reper.ui.boss.WriteNotiFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bossbtn.setOnClickListener {
+            val fragment = BossFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fram_fragment, fragment)
+                .addToBackStack(null)
+                .commit()
+
+
+            invisableBtn()
         }
 
-        // 정언이 왔다감'
+
+        binding.recipebtn.setOnClickListener {
+            val fragment = RecipeManageFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fram_fragment, fragment)
+                .addToBackStack(null)
+                .commit()
+
+
+            invisableBtn()
+        }
+
+
+        binding.notibtn.setOnClickListener {
+            val fragment = NoticeManageFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fram_fragment, fragment)
+                .addToBackStack(null)
+                .commit()
+
+
+            invisableBtn()
+        }
+
+
+        binding.notiwritebtn.setOnClickListener {
+            val fragment = WriteNotiFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fram_fragment, fragment)
+                .addToBackStack(null)
+                .commit()
+
+
+            invisableBtn()
+        }
+
+
+
+
+
+    }
+    fun invisableBtn(){
+        binding.bossbtn.visibility = View.GONE
+        binding.recipebtn.visibility = View.GONE
+        binding.notibtn.visibility = View.GONE
+        binding.notiwritebtn.visibility = View.GONE
+
     }
 }
