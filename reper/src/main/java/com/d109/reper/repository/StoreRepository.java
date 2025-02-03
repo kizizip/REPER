96a437562c9d8db6c5f7a19e1980303de2f79919
PARK Jaeyoung <estel2005@naver.com>
2025-02-03 12:50:24 +0900

    [feat] back - 사장님이 운영하는 모든 매장 정보 조회 구현

---
+++ reper/src/main/java/com/d109/reper/repository/StoreRepository.java
@@ -0,0 +1,15 @@
+package com.d109.reper.repository;
+
+import com.d109.reper.domain.Store;
+import com.d109.reper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+    List<Store> findAllByOwner(User owner);
+
+}
+
---
+++ reper/src/main/java/com/d109/reper/service/StoreService.java
@@ -0,0 +1,40 @@
+package com.d109.reper.service;
+
+import com.d109.reper.domain.Store;
+import com.d109.reper.domain.User;
+import com.d109.reper.domain.UserRole;
+import com.d109.reper.repository.StoreRepository;
+import com.d109.reper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class StoreService {
+
+    private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
+
+    // 사장님이 가진 모든 매장 조회
+    public List<Store> findStores(Long userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("userId는 필수입니다.");
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User Not Found"));
+
+        if (!user.getRole().equals(UserRole.OWNER)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "해당 user는 사장님이 아닙니다.");
+        }
+
+        return storeRepository.findAllByOwner(user);
+    }
+
+}
