cd6c4d43a4b6e5fc8815bf328156ac8e731154b2
PARK Jaeyoung <estel2005@naver.com>
2025-02-09 01:59:59 +0900

    [feat] back - 매장 CD ELK DB에 반영

--- reper/src/main/java/com/d109/reper/elasticsearch/StoreSearchRepository.java
+++ reper/src/main/java/com/d109/reper/elasticsearch/StoreSearchRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StoreSearchRepository extends ElasticsearchRepository<StoreDocument, Long> {
 
     List<StoreDocument> findByStoreNameContaining(String storeName, Pageable pageable);
+
+    Optional<StoreDocument> findByStoreId(Long storeId);
 }
