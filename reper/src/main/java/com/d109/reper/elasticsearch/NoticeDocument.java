a431d9afb9ac02fe49cefcfbfc98cc6115b60149
PARK Jaeyoung <estel2005@naver.com>
2025-02-09 01:35:01 +0900

    [feat] back - 공지 CUD될 때 ELK DB에 반영

--- reper/src/main/java/com/d109/reper/elasticsearch/NoticeDocument.java
+++ reper/src/main/java/com/d109/reper/elasticsearch/NoticeDocument.java
@@ -10,7 +10,6 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
