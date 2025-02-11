fd2575405ba399b30d141df5c246378f0e808a1c
PARK Jaeyoung <estel2005@naver.com>
2025-02-11 13:10:36 +0900

    [feat] - 매장 검색 fuzzy 설정 추가

--- reper/src/main/java/com/d109/reper/elasticsearch/StoreDocument.java
+++ reper/src/main/java/com/d109/reper/elasticsearch/StoreDocument.java
@@ -3,15 +3,22 @@ package com.d109.reper.elasticsearch;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.*;
 
 @Getter
 @Setter
 @Document(indexName = "stores_index")
+@Setting(settingPath = "recipes-index-settings.json")
 public class StoreDocument {
 
     @Id
     private Long storeId;
 
+    @MultiField(
+            mainField = @Field(type = FieldType.Text, analyzer = "nori_analyzer", searchAnalyzer = "nori_analyzer"),
+            otherFields = {
+                    @InnerField(suffix = "ngram", type = FieldType.Text, analyzer = "nori_edge_ngram_analyzer", searchAnalyzer = "nori_edge_ngram_analyzer")
+            }
+    )
     private String storeName;
 }
